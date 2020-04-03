9bb0b071fa32e83950622d28f03fb5a665e3186f
eduard-de <63105337+eduard-de@users.noreply.github.com>
2020-04-03 18:10:02 +0300

    Update ___SuppressConfig.js

--- ___SuppressConfig.js
+++ ___SuppressConfig.js
@@ -8,7 +8,7 @@ class SuppressConfig {
 
     static var TableSuppressValue = 5;       // Min number of answers in the Aggregated Table
     static var VerbatimSuppressValue = 1;     // Min number of answers in the Verbatim Table
-    static var CommentSuppressValue = 1;      // Min number of answers in the Hitlist
+    static var CommentSuppressValue = 3;      // Min number of answers in the Hitlist
     static var CategoricalSuppressValue = 1; // Min number of answers for cards on the Categorical page
     static var BenchamrkSuppressValue = 3; // Min number of answers for benchmark value to be shown in Results table
 
