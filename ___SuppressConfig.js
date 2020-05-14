691cb5a319c54e31e926e0d8385e6565c8f9b2f2
Namioko <galaida_anna@inbox.ru>
2020-05-14 16:35:18 +0300

    Set all zeros

--- ___SuppressConfig.js
+++ ___SuppressConfig.js
@@ -7,13 +7,13 @@ class SuppressConfig {
     static var ReportBaseSuppressValue = 0;  // Min number of responses (response = question specified below in Survey Config -> Response: {qId: 'status', codes: ['complete']})
 
     static var TableSuppressValue = 0;       // Min number of answers in the Aggregated Table
-    static var VerbatimSuppressValue = 1;     // Min number of answers in the Verbatim Table
+    static var VerbatimSuppressValue = 0;     // Min number of answers in the Verbatim Table
     static var CommentSuppressValue = 0;      // Min number of answers in the Hitlist
-    static var CategoricalSuppressValue = 1; // Min number of answers for cards on the Categorical page
-    static var BenchamrkSuppressValue = 3; // Min number of answers for benchmark value to be shown in Results table
+    static var CategoricalSuppressValue = 0; // Min number of answers for cards on the Categorical page
+    static var BenchamrkSuppressValue = 0; // Min number of answers for benchmark value to be shown in Results table
 
     // minGap: min difference between neighbour units
     // unitSufficientBase: min number of the responses when a unit is always shown irrespective of <minGap> requirement
-    static var HierarchySuppress = { minGap: 100, unitSufficientBase: 5};
+    static var HierarchySuppress = { minGap: 100, unitSufficientBase: 0};
 
 }
