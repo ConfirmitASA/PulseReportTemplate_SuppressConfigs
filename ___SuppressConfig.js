bd8cfb9d590169b1f0884a29cace5d9be2406fcd
EkaterinaT-CIT <EkaterinaT-CIT@users.noreply.github.com>
2020-05-19 14:54:48 +0300

    Update ___SuppressConfig.js

--- ___SuppressConfig.js
+++ ___SuppressConfig.js
@@ -12,8 +12,16 @@ class SuppressConfig {
     static var CategoricalSuppressValue = 1; // Min number of answers for cards on the Categorical page
     static var BenchamrkSuppressValue = 3; // Min number of answers for benchmark value to be shown in Results table
 
-    // minGap: min difference between neighbour units
+     //hierarchical suppress
     // unitSufficientBase: min number of the responses when a unit is always shown irrespective of <minGap> requirement
+    // minGap: min difference between neighbour units
+	// a) parent - selfUnitBase
+  	// b) parent - allSiblingsBase
+    // algorithm:
+	// 1) several nodes are selected -> show data, otherwise check further 
+    // 2) selfUnitBase > unitSufficientBase -> show data, otherwise check further
+  	// 3) parentBase - selfUnitBase > minGap -> show data, otherwise check further
+  	// 4) parentBase - allSiblingsBase > minGap -> show data, otherwise check further
     static var HierarchySuppress = { minGap: 100, unitSufficientBase: 5};
 
 }
