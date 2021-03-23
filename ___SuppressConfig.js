2e798b96391c91ab7a09d839e591570133c75a5e
EkaterinaT-CIT <EkaterinaT-CIT@users.noreply.github.com>
2021-03-23 16:21:29 +0300

    Update ___SuppressConfig.js
    RBI-216 - restore 5 limit for verbatims

--- ___SuppressConfig.js
+++ ___SuppressConfig.js
@@ -7,7 +7,7 @@ class SuppressConfig {
     static var ReportBaseSuppressValue = 5;  // Min number of responses (response = question specified below in Survey Config -> Response: {qId: 'status', codes: ['complete']})
 
     static var TableSuppressValue = 5;        // Min number of answers in the Aggregated Table
-    static var VerbatimSuppressValue = 10;     // Min number of answers in the Verbatim Table
+    static var VerbatimSuppressValue = 5;     // Min number of answers in the Verbatim Table
     static var CommentSuppressValue = 20;      // Min number of answers in the Hitlist
     static var CategoricalSuppressValue = 5; // Min number of answers for cards on the Categorical page
     static var BenchamrkSuppressValue = 5; // Min number of answers for benchmark value to be shown in Results table
